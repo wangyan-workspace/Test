8a4ce5560e99b09b522eb8df08da661619331da2
wangyan-workspace <2107014530@qq.com>
2020-10-30 16:44:12 +0800

    自定义插件，组件，过滤器

---
+++ vue-exercise/plugins.js
@@ -0,0 +1,26 @@
+(function(){
+    const MyPlugin = {};
+    MyPlugin.install = function (Vue, options) {
+        // 1. 添加全局方法或 property
+        Vue.myGlobalMethod = function () {
+          // 逻辑...
+          alert("MyPlugin.myGlobalMethod方法被调用了")
+        }
+      
+        // 2. 添加全局资源
+        Vue.directive('my-directive', {
+          bind (el, binding) {
+            // 逻辑...
+            el.innerHTML = "MyPlugin.my-directive指令被调用了" + binding.value
+          }
+        })
+      
+      
+        // 3. 添加实例方法
+        Vue.prototype.$myMethod = function (value) {
+          // 逻辑...
+          alert("vue实例方法myMethod被调用了：" + value)
+        }
+      }
+      window.MyPlugin = MyPlugin;
+})()
