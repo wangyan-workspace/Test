d4e9b93b611ab5991237bba28c40515ae1bc3cae
wangyan-workspace <2107014530@qq.com>
2021-01-11 20:43:33 +0800

    typescript的类和接口

--- TYPESCRIPT/3_class_interface.js
+++ TYPESCRIPT/3_class_interface.js
@@ -38,8 +38,59 @@ let p1 = new Person('lisi');
 // p1.sayHello();
 // p1.height = '180cm';//只读的，不可以修改
 // console.log(p1.height);
-p1.setGender('male');
-console.log(p1.getGender('I love you'));
+// p1.setGender('male');
+// console.log(p1.getGender('I love you'));
 // p1.gender = '男';
 // console.log(p1.gender);
-console.log(Person.legs); //对于静态数据不用重新 new一个对象实例，可以直接通过类名访问到
+// console.log(Person.legs); //对于静态数据不用重新 new一个对象实例，可以直接通过类名访问到
+// 抽象类和继承
+// 设计抽象的父类，一定是用来做继承的
+// 抽象类中可以定义抽象的方法，也可以定义非抽象类的方法
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+}
+class Dog extends Animal {
+    shout() {
+        return '汪汪';
+    }
+}
+class Cat extends Animal {
+    shout() {
+        return '喵喵';
+    }
+}
+let dog = new Dog('旺财');
+let cat = new Cat('咪咪');
+class Monkey extends Animal {
+    shout() {
+        return '吱吱';
+    }
+}
+class MonkeyKing extends Monkey {
+    fly() {
+        return '我可以驾筋斗云飞';
+    }
+}
+class MachineCat extends Cat {
+    fly() {
+        return '我可以在头上插个小风扇飞！';
+    }
+}
+// let wukong = new MonkeyKing('悟空');
+// console.log(wukong.shout());
+// console.log(wukong.fly());
+// let doraamon = new MachineCat('哆啦A梦');
+// console.log(doraamon.shout());
+// console.log(doraamon.fly());
+// **多态：在统一标准下的多种形态**
+// let flier :IFly = new MonkeyKing('悟空');
+// console.log(flier.fly());
+// let flier :IFly = new MachineCat('哆啦A梦');
+// console.log(flier.fly());
+function fly(flier) {
+    console.log(flier.fly());
+}
+fly(new MonkeyKing('悟空'));
+// fly(new MachineCat('哆啦A梦'));
--- TYPESCRIPT/3_class_interface.ts
+++ TYPESCRIPT/3_class_interface.ts
@@ -46,9 +46,84 @@ let p1 = new Person('lisi');
 // p1.sayHello();
 // p1.height = '180cm';//只读的，不可以修改
 // console.log(p1.height);
-p1.setGender('male');
-console.log(p1.getGender('I love you'));
+// p1.setGender('male');
+// console.log(p1.getGender('I love you'));
 // p1.gender = '男';
 // console.log(p1.gender);
 
-console.log(Person.legs); //对于静态数据不用重新 new一个对象实例，可以直接通过类名访问到
+// console.log(Person.legs); //对于静态数据不用重新 new一个对象实例，可以直接通过类名访问到
+
+// 抽象类和继承
+// 设计抽象的父类，一定是用来做继承的
+// 抽象类中可以定义抽象的方法，也可以定义非抽象类的方法
+abstract class Animal{
+    name: string;
+    constructor(name: string){
+        this.name = name;
+    }
+    // 抽象的方法一定要在抽象类中，抽象类中肯定有抽象的方法，抽象类中抽象的方法不需要写方法体，在子类中具体实现抽象的方法
+    abstract shout(): string; //抽象的方法一定要在子类中被重写
+}
+class Dog extends Animal{
+    shout(){
+        return '汪汪';
+    }
+}
+class Cat extends Animal{
+    shout(){
+        return '喵喵';
+    }
+}
+
+let dog = new Dog('旺财');
+let cat = new Cat('咪咪');
+// console.log(dog.shout());
+// console.log(cat.shout());
+
+// let animal = new Animal(); //抽象类不能被实例化
+
+// 接口（接口本身是抽象的,接口中的所有的方法也都是抽象的）
+// 如果实现一个接口，就要实现接口里的全部方法
+interface IFly{
+    fly():string;
+}
+
+class Monkey extends Animal{
+    shout(){
+        return '吱吱';
+    }
+}
+
+class MonkeyKing extends Monkey implements IFly{
+    fly(){
+        return '我可以驾筋斗云飞';
+    }
+}
+
+class MachineCat extends Cat implements IFly{
+    fly(){
+        return '我可以在头上插个小风扇飞！';
+    }
+}
+
+// let wukong = new MonkeyKing('悟空');
+// console.log(wukong.shout());
+// console.log(wukong.fly());
+
+// let doraamon = new MachineCat('哆啦A梦');
+// console.log(doraamon.shout());
+// console.log(doraamon.fly());
+
+// **多态：在统一标准下的多种形态**
+// let flier :IFly = new MonkeyKing('悟空');
+// console.log(flier.fly());
+
+// let flier :IFly = new MachineCat('哆啦A梦');
+// console.log(flier.fly());
+
+function fly(flier: IFly){
+    console.log(flier.fly());
+}
+
+fly(new MonkeyKing('悟空'));
+// fly(new MachineCat('哆啦A梦'));
